b89ef8fa55faf089847457a53e0a9028dd89d94d
nisrinahnfhh <nisrinahnfhh@gmail.com>
2025-03-18 15:32:46 +0700

    Menambahkan kode ke file For

--- nanalooping.cpp
+++ nanalooping.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    //Perulangan for untuk pencacah naik
+    int i;
+    cout << "Perulangan For Pencacah Naik" << endl;
+    for (i = 0; i <= 4; i++){
+        cout << "Nilai i : " << i
+        << "Lari keliling lapangan" << endl;
+    }
 
+    cout << "Nilai i terakhir : " << i << endl;
+    cout << endl;
+
+    // Perulangan for untuk pencacah turun
+    cout << "Perulangan For Pencacah Turun" << endl;
+    for (i = 4; i >= 0; i--){
+        cout << "Nilai i : " << i
+        << " Lari keliling lapangan" << endl;
+}
 }
