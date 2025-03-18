2cd2ea27f1f497bb3f62186be29713e7451f0c79
nisrinahnfhh <nisrinahnfhh@gmail.com>
2025-03-18 16:46:37 +0700

    Membuat file ke program DoWhile

--- nanalooping2.cpp
+++ nanalooping2.cpp
@@ -4,5 +4,31 @@
 using namespace std;
 
 int main (){
-   
-}
+    //Perulangan While
+    cout << "Perulangan While" << endl;
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+    srand(time(0));
+    x = rand() % 10;
+
+    cout << "Nilai x awal : " << x << endl;
+    while(x < 7){
+        cout << "Nilai x : " << x << " Lari Keliling Lapangan" << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Nilai x akhir : " << x << endl;
+    cout << "Jumlah perulangan : " << perulanganWhile << endl;
+    cout << endl;
+
+    // Perulangan Do While
+    cout << "Perulangan Do While" << endl;
+    do{
+        cout << "Nilai x : " << x << "Lari Keliling Lapangan" << endl;
+        x = rand() % 10;
+        perulanganDoWhile++;
+    }while (x < 7);
+    cout << "Nilai x akhir : " << x << endl;
+    cout << "Jumlah perulangan : " << perulanganDoWhile<< endl;
+    }
